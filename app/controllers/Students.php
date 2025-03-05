b7560ac89525e6d3e73954ad2049f7847323ecdb
aryndraa <aryacoder1102@gmail.com>
2025-03-06 03:05:05 +0800

    initial commit

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -2,21 +2,6 @@
 
 class Students extends Controller
 {
-    public function show($id) {
-        $data['judul'] = 'Detail';
-        $data['students'] = $this->model('Student')->getStudentById($id);
-        $this->view('templates/header', $data);
-        $this->view('students/show', $data);
-        $this->view('templates/footer');
-    }
-
-    public function create() {
-        $data['judul'] = 'Add Student';
-        $this->view('templates/header', $data);
-        $this->view('students/create');
-        $this->view('templates/footer');
-    }
-
     public function store() {
         if($this->model('Student')->addStudent($_POST) > 0 ) {
             Flasher::setFlash('Success', 'Create Success', 'green');
@@ -27,15 +12,6 @@ class Students extends Controller
             header('location: ' . BASEURL );
             exit;
         }
-    }
-
-    public function edit($id) {
-        $data['judul']    = 'Edit Student';
-        $data['students'] = $this->model('Student')->getStudentById($id);
-
-        $this->view('templates/header', $data);
-        $this->view('students/edit', $data);
-        $this->view('students/footer');
     }
 
     public function update() {
